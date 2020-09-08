9bc92d5b5f457fbcd3183b4674969476a9cb1d13
CWTBT <lw03232@gmail.com>
2020-09-08 14:57:28 -0500

    Initial commit (Basic layout done)

---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 2,
+            child: Container(
+              color: Colors.lightGreen[100],
+              width: MediaQuery.of(context).size.width,
+              child: Center(
+                child: Text("I am a Driver.")
+              )
+            )
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+              color: Colors.lightGreen[600],
+              width: MediaQuery.of(context).size.width,
+              child: Center(
+                child: Text("I am a Rider.")
+              )
+            )
+          )
+        ]
+      ),
+    );
+  }
+}
