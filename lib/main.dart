37a56a360c45f03358c15f8ff7ed6a6293da2362
CWTBT <lw03232@gmail.com>
2020-09-08 20:52:16 -0500

    Prepared for multiple states, moved funcs inside class

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Uber Companion',
       theme: ThemeData(
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.lightGreen,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Uber Companion'),
     );
   }
 }
@@ -28,73 +28,94 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  AppState _state = AppState.USER_TYPE_MENU;
+
+  void initState() {
+    super.initState();
+  }
+
+  void dispose() {
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    double screenWidth = MediaQuery.of(context).size.width;
+    double _screenWidth = MediaQuery.of(context).size.width;
+    switch(_state) {
+      case AppState.USER_TYPE_MENU:
+        return _buildUserTypeMenu(_screenWidth);
+    }
+  }
 
+  Widget _buildUserTypeMenu(double screenWidth) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
       body: Column(
-        children: <Widget>[
-          _buildButton(screenWidth, UserType.DRIVER),
-          _buildButton(screenWidth, UserType.PASSENGER),
-        ]
+          children: <Widget>[
+            _buildButton(screenWidth, UserType.DRIVER),
+            _buildButton(screenWidth, UserType.PASSENGER),
+          ]
       ),
     );
   }
-}
 
-Widget _buildButton(double screenWidth, UserType user) {
-  return Expanded (
-      flex: 2,
-      child: new GestureDetector(
-          onTap: (){
-            _printUserType(user);
-          },
-          child: new Container(
-              color: _getColorForUserType(user),
-              width: screenWidth,
-              child: Center(
-                child: _getUserTypeText(user),
-              )
-          )
-      )
-  );
-}
-
-void _printUserType(UserType user) {
-  switch(user) {
-    case UserType.DRIVER:
-      print("I am a DRIVER");
-      break;
-    case UserType.PASSENGER:
-      print("I am a PASSENGER");
-      break;
+  Widget _buildButton(double screenWidth, UserType user) {
+    return Expanded (
+        flex: 2,
+        child: new GestureDetector(
+            onTap: (){
+              _printUserType(user);
+            },
+            child: new Container(
+                color: _getColorForUserType(user),
+                width: screenWidth,
+                child: Center(
+                  child: _getUserTypeText(user),
+                )
+            )
+        )
+    );
   }
-}
 
-Widget _getUserTypeText(UserType user) {
-  switch(user) {
-    case UserType.DRIVER:
-      return Text("I am a DRIVER");
-    case UserType.PASSENGER:
-      return Text("I am a PASSENGER");
+  void _printUserType(UserType user) {
+    switch(user) {
+      case UserType.DRIVER:
+        print("I am a DRIVER");
+        break;
+      case UserType.PASSENGER:
+        print("I am a PASSENGER");
+        break;
+    }
   }
-}
 
-Color _getColorForUserType(UserType user) {
-  switch(user) {
-    case UserType.DRIVER:
-      return Colors.lightGreen[100];
-    case UserType.PASSENGER:
-      return Colors.lightGreen[600];
+  Widget _getUserTypeText(UserType user) {
+    switch(user) {
+      case UserType.DRIVER:
+        return Text("I am a DRIVER");
+      case UserType.PASSENGER:
+        return Text("I am a PASSENGER");
+    }
+  }
+
+  Color _getColorForUserType(UserType user) {
+    switch(user) {
+      case UserType.DRIVER:
+        return Colors.lightGreen[100];
+      case UserType.PASSENGER:
+        return Colors.lightGreen[600];
+    }
   }
 }
 
 enum UserType{
   DRIVER,
   PASSENGER
+}
+
+enum AppState{
+  USER_TYPE_MENU,
+  DRIVER_MENU,
+  PASSENGER_MENU
 }
